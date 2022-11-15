c9b3eb03c83fd74de16af44c2efafe9ef0f87669
Juuso Haavisto <juuso@mail.com>
2022-11-15 12:28:50 +0200

    steam: add gamescope-compatible steam as an overlay

---
+++ overlays/steam.nix
@@ -0,0 +1,17 @@
+# https://github.com/NixOS/nixpkgs/issues/162562#issuecomment-1229444338
+self: super: {
+  steam = super.steam.override {
+    extraPkgs = pkgs: with pkgs; [
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXinerama
+      xorg.libXScrnSaver
+      libpng
+      libpulseaudio
+      libvorbis
+      stdenv.cc.cc.lib
+      libkrb5
+      keyutils
+    ];
+  };
+}
