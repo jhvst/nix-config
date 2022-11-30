5f479804a50e880ab979084c06c85d3e09d4c69f
Juuso Haavisto <juuso@mail.com>
2022-11-30 14:14:57 +0200

    mesa: use 22.3.0-rc4

--- overlays/mesa.nix
+++ overlays/mesa.nix
@@ -1,17 +1,17 @@
 self: super: with super.lib; {
   mesa = super.mesa.overrideAttrs (old: {
-    version = "trunk";
+    version = "22.3.0-rc4";
     src = super.fetchFromGitHub {
       owner = "Mesa3D";
       repo = "mesa";
-      rev = "71c4543af1ad7b2b51f18140373b8756c1631d07";
+      rev = "mesa-22.3.0-rc4";
       # If you don't know the hash, the first time, set:
       # sha256 = "0000000000000000000000000000000000000000000000000000";
       # then nix will fail the build with such an error message:
       # hash mismatch in fixed-output derivation '/nix/store/m1ga09c0z1a6n7rj8ky3s31dpgalsn0n-source':
       # wanted: sha256:0000000000000000000000000000000000000000000000000000
       # got:    sha256:173gxk0ymiw94glyjzjizp8bv8g72gwkjhacigd1an09jshdrjb4
-      sha256 = "w4J59afHu8OSy1DqDtTEEsm1ELfZk8LDdNPsSpyjN8U=";
+      sha256 = "UO4+tWlymP3Pqx9HHv9nNkZz7tBCeOpjKHTC7+UNQZA=";
     };
     patches = [
       ./patches/opencl.patch
