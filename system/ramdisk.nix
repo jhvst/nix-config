35fb4e983c1c92a5cc28f87445fda357ad639c8a
Juuso Haavisto <juuso@mail.com>
2022-12-11 17:25:53 +0200

    ramdisk: remove timeout + fmt

--- system/ramdisk.nix
+++ system/ramdisk.nix
@@ -1,60 +1,57 @@
-{
-    config,
-    lib,
-    ...
+{ config
+, lib
+, ...
 }:
 {
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-    # These kmodules are implicit requirements of netboot
-    boot.initrd.availableKernelModules = [ "squashfs" "overlay" ];
-    boot.initrd.kernelModules = [ "loop" "overlay" ];
+  # These kmodules are implicit requirements of netboot
+  boot.initrd.availableKernelModules = [ "squashfs" "overlay" ];
+  boot.initrd.kernelModules = [ "loop" "overlay" ];
 
-    fileSystems."/" = lib.mkImageMediaOverride {
-      fsType = "tmpfs";
-      options = [ "mode=0755" ];
-    };
+  fileSystems."/" = lib.mkImageMediaOverride {
+    fsType = "tmpfs";
+    options = [ "mode=0755" ];
+  };
 
-    # In stage 1, mount a tmpfs on top of /nix/store (the squashfs
-    # image) to make this a live CD.
-    fileSystems."/nix/.ro-store" = lib.mkImageMediaOverride {
-      fsType = "squashfs";
-      device = "../nix-store.squashfs";
-      options = [ "loop" ];
-      neededForBoot = true;
-    };
+  # In stage 1, mount a tmpfs on top of /nix/store (the squashfs
+  # image) to make this a live CD.
+  fileSystems."/nix/.ro-store" = lib.mkImageMediaOverride {
+    fsType = "squashfs";
+    device = "../nix-store.squashfs";
+    options = [ "loop" ];
+    neededForBoot = true;
+  };
 
-    fileSystems."/nix/.rw-store" = lib.mkImageMediaOverride {
-      fsType = "tmpfs";
-      options = [ "mode=0755" ];
-      neededForBoot = true;
-    };
+  fileSystems."/nix/.rw-store" = lib.mkImageMediaOverride {
+    fsType = "tmpfs";
+    options = [ "mode=0755" ];
+    neededForBoot = true;
+  };
 
-    fileSystems."/nix/store" = lib.mkImageMediaOverride {
-      fsType = "overlay";
-      device = "overlay";
-      options = [
-        "lowerdir=/nix/.ro-store"
-        "upperdir=/nix/.rw-store/store"
-        "workdir=/nix/.rw-store/work"
-      ];
+  fileSystems."/nix/store" = lib.mkImageMediaOverride {
+    fsType = "overlay";
+    device = "overlay";
+    options = [
+      "lowerdir=/nix/.ro-store"
+      "upperdir=/nix/.rw-store/store"
+      "workdir=/nix/.rw-store/work"
+    ];
 
-      depends = [
-        "/nix/.ro-store"
-        "/nix/.rw-store/store"
-        "/nix/.rw-store/work"
-      ];
-    };
+    depends = [
+      "/nix/.ro-store"
+      "/nix/.rw-store/store"
+      "/nix/.rw-store/work"
+    ];
+  };
 
-    boot.loader.timeout = 10;
-
-    boot.postBootCommands =
-      ''
-        # After booting, register the contents of the Nix store in the Nix database in the tmpfs.
-        ${config.nix.package}/bin/nix-store --load-db < /nix/store/nix-path-registration
-        # nixos-rebuild also requires a "system" profile and an /etc/NIXOS tag.
-        touch /etc/NIXOS
-        ${config.nix.package}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
-      '';
+  boot.postBootCommands =
+    ''
+      # After booting, register the contents of the Nix store in the Nix database in the tmpfs.
+      ${config.nix.package}/bin/nix-store --load-db < /nix/store/nix-path-registration
+      # nixos-rebuild also requires a "system" profile and an /etc/NIXOS tag.
+      touch /etc/NIXOS
+      ${config.nix.package}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
+    '';
 }
