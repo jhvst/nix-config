d1ae2be2b40c4fa9a534d400de8269e1dedbd181
Juuso Haavisto <juuso@mail.com>
2022-12-28 21:45:23 +0200

    vkroots: init

---
+++ pkgs/vkroots/default.nix
@@ -0,0 +1,23 @@
+{ stdenv
+, fetchFromGitHub
+, meson
+, pkg-config
+, ninja
+, ...
+}:
+
+
+stdenv.mkDerivation rec {
+  pname = "vkroots";
+  version = "a7ee30d527231f7b8798c20c22345a79e8b8caac";
+  src = fetchFromGitHub {
+    owner = "Scrumplex";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-dwKqHDuDBFXKp+X6H9vVplf546sI1j6WulxWN99xaBM=";
+  };
+  meta.homepage = "https://github.com/Joshua-Ashton/vkroots";
+
+  nativeBuildInputs = [ meson pkg-config ninja ];
+
+}
