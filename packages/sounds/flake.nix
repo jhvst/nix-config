1fb14c953313258d68d7d188144577ca32c9b84c
Juuso Haavisto <juuso@ponkila.com>
2023-08-20 17:05:27 +0100

    packages/sounds: init

---
+++ packages/sounds/flake.nix
@@ -0,0 +1,51 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
+  };
+
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , flake-parts
+    , ...
+    }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      imports = [
+        inputs.flake-root.flakeModule
+      ];
+
+      perSystem = { pkgs, lib, config, system, ... }: rec {
+
+        devShells.default = pkgs.mkShell {
+          packages = packages.default.nativeBuildInputs;
+        };
+
+        packages.default = pkgs.symlinkJoin rec {
+          sounds-script = (pkgs.writeScriptBin "sounds" (builtins.readFile ./sounds.sh)).overrideAttrs (old: {
+            src = ./.;
+            buildCommand = ''
+              ${old.buildCommand}
+              patchShebangs $out
+            '';
+          });
+
+          name = "sounds";
+          paths = [ sounds-script ] ++ nativeBuildInputs;
+          buildInputs = with pkgs; [ makeWrapper ];
+          nativeBuildInputs = with pkgs; [ jq curl unixtools.xxd binutils ];
+          postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+        };
+      };
+    };
+}
---
+++ packages/sounds/sounds.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl -sX POST --data '{"jsonrpc":"2.0","method":"eth_getLogs","params":[{"topics":["0x81fb8daf8a05fc760e25f1447b0ca819bcf138a168ec6c1aaa0bd62b170bf32a"], "fromBlock": 17695077, "toBlock": 17795079}],"id":1}' -H 'Content-Type: application/json' http://192.168.100.10:8545 | jq -r '.result[] | .data' | xxd -r -p | strings | grep "://" | cut -c2- | sed 's/:\/\//\ /g' | awk '{ print "https://" $1 "/" $2 "/0" }' | grep "https://ar/" | sed 's/ar/arweave.net/g' > links.txt
+
+cat links.txt | xargs curl -sLI | grep -E "HTTP" | grep -v "HTTP/2 302" | cut -d ' ' -f2 > codes.txt
+
+paste codes.txt links.txt | grep "200" | cut -f2 | xargs curl -sL | jq -r '.losslessAudio' | grep "ar://" | sed 's/:\/\//\ /g' | awk '{ print "https://" $1 "/" $2 "/0" }' | grep "https://ar/" | sed 's/ar/arweave.net/g'
