e57a790401e6a5bd6900443765301d06c5055fcc
Juuso Haavisto <juuso@ponkila.com>
2024-01-07 17:48:07 +0000

    minimal: make bootable

--- nixosConfigurations/minimal/default.nix
+++ nixosConfigurations/minimal/default.nix
@@ -1,18 +1,33 @@
 { config, pkgs, lib, ... }: with lib; {
 
-  documentation.enable = false;
-
-  boot.initrd.availableKernelModules = [ "squashfs" "overlay" "e1000" ];
+  time.timeZone = "Europe/Helsinki";
+  boot.initrd.availableKernelModules = [ "squashfs" "overlay" ];
   boot.initrd.kernelModules = [ "loop" "overlay" ];
 
   boot.kernelParams = [
     "boot.shell_on_fail"
   ];
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_latest);
+
+  users.users.juuso = {
+    isNormalUser = true;
+    group = "juuso";
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNMKgTTpGSvPG4p8pRUWg1kqnP9zPKybTHQ0+Q/noY5+M6uOxkLy7FqUIEFUT9ZS/fflLlC/AlJsFBU212UzobA= ssh@secretive.sandbox.local"
+    ];
+  };
+  users.groups.juuso = { };
+
+  security.sudo = {
+    enable = lib.mkDefault true;
+    wheelNeedsPassword = lib.mkForce false;
+  };
 
   fileSystems."/" = lib.mkImageMediaOverride {
     fsType = "tmpfs";
     options = [ "mode=0755" ];
   };
+
   boot.loader.grub.enable = false;
-  boot.initrd.network.enable = true;
 }
