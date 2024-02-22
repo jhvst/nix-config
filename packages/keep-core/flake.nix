4361e85b94d055dd5ce2cd628d6f24753ca574e4
Juuso Haavisto <juuso@ponkila.com>
2024-02-22 12:04:15 +0000

    keep-core: v2.0.0-m6 -> v2.0.0-m7

--- packages/keep-core/flake.nix
+++ packages/keep-core/flake.nix
@@ -1,7 +1,7 @@
 {
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/c2c0373ae7abf25b7d69b2df05d3ef8014459ea3";
+    nixpkgs.url = "github:nixos/nixpkgs/23.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-root.url = "github:srid/flake-root";
   };
@@ -28,21 +28,20 @@
 
         packages.default = packages.keep-core;
 
-        packages.keep-core = pkgs.buildGoModule
-          rec {
+        packages.keep-core = pkgs.buildGoModule rec {
 
-            name = "keep-core";
+          name = "keep-core";
 
-            src = pkgs.fetchFromGitHub {
-              owner = "keep-network";
-              repo = name;
-              rev = "v2.0.0-m6";
-              hash = "sha256-XMMp1dE2BBsIqoqSl9qUUyjYwSy24fQM6WqdftgzYn8=";
-            };
-
-            vendorHash = "sha256-L+5rfneAVZkssUzpoPpc6AmgmMGkbrfQccHLg237EXo=";
-
+          src = pkgs.fetchFromGitHub {
+            owner = "keep-network";
+            repo = name;
+            rev = "v2.0.0-m7";
+            hash = "sha256-lRcfebsycA1Fs5zil57sZW0ll5ZRQ2May4fOfLmBogY=";
           };
+
+          vendorHash = "sha256-ZWQmKYLZYIvbIrhUJU3S27Qmjpp3Jjchqi0Sz7CoyLk=";
+
+        };
       };
     };
 }
