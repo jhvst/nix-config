8928831c9d4368698a1c44621ffbe3bb62751e20
Juuso Haavisto <juuso@ponkila.com>
2025-02-25 14:44:20 +0000

    nixosModules/juuso: git improvements see: https://blog.gitbutler.com/how-git-core-devs-configure-git/

--- nixosModules/juuso/default.nix
+++ nixosModules/juuso/default.nix
@@ -9,15 +9,34 @@
       enable = true;
       lfs.enable = true;
       config = {
+        branch.sort = "-committerdate";
+        column.ui = "auto";
         commit.gpgsign = true;
+        commit.verbose = true;
+        diff.algorithm = "histogram";
+        diff.colorMoved = "plain";
+        diff.mnemonicPrefix = true;
+        diff.renames = true;
+        fetch.all = true;
+        fetch.prune = true;
+        fetch.pruneTags = true;
         gpg.format = "ssh";
         gpg.ssh.program = "${pkgs.openssh}/bin/ssh-keygen";
-        tag.gpgsign = true;
-        user.signingkey = "/home/juuso/.ssh/id_ed25519_sk_rk_starlabs";
-        user.name = "Juuso Haavisto";
-        user.email = "juuso@ponkila.com";
         http.postbuffer = 524288000; # https://stackoverflow.com/questions/22369200/git-pull-push-error-rpc-failed-result-22-http-code-408
+        init.defaultBranch = "main";
+        merge.conflictstyle = "zdiff3";
+        pull.rebase = true;
         push.autosetupremote = true;
+        push.default = "simple";
+        push.followTags = true;
+        rebase.autoSquash = true;
+        rebase.autoStash = true;
+        rebase.updateRefs = true;
+        tag.gpgsign = true;
+        tag.sort = "version:refname";
+        user.email = "juuso@ponkila.com";
+        user.name = "Juuso Haavisto";
+        user.signingkey = "/home/juuso/.ssh/id_ed25519_sk_rk_starlabs";
       };
     };
   };
