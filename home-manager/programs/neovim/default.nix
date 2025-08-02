86718cd65bde4a415895f35b793f2cf87def594c
Juuso Haavisto <juuso@ponkila.com>
2025-08-02 12:33:40 +0100

    neovim: insert_final_newline true

--- home-manager/programs/neovim/default.nix
+++ home-manager/programs/neovim/default.nix
@@ -15,7 +15,7 @@
         charset = "utf-8";
         end_of_line = "lf";
         trim_trailing_whitespace = true;
-        insert_final_newline = false;
+        insert_final_newline = true;
         max_line_width = 78;
         indent_style = "space";
         indent_size = 2;
