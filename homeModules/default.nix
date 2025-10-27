4357d958499e3b66bab35533a02abd55bf55d282
Juuso Haavisto <juuso@ponkila.com>
2025-10-27 15:08:10 +0000

    homeModules/default: add mergiraf

--- homeModules/default.nix
+++ homeModules/default.nix
@@ -6,7 +6,7 @@
   config = {
     home-manager.users.juuso = _: {
       home.stateVersion = config.system.stateVersion;
-
+      programs.mergiraf.enable = true;
       services.mako.enable = true;
     };
   };
