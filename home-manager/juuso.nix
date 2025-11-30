f73eae44dd5886cd9b5dbe5fa46879cab9e298aa
Juuso Haavisto <juuso@ponkila.com>
2025-11-30 22:02:17 +0200

    home-manager/juuso: combine programs closures

--- home-manager/juuso.nix
+++ home-manager/juuso.nix
@@ -5,25 +5,9 @@
 {
 
   home-manager.backupFileExtension = "backup";
-  home-manager.users.juuso = { pkgs, ... }: {
+  home-manager.users.juuso = { pkgs, ... }: with config.home-manager.users.juuso; {
 
     home.stateVersion = config.system.stateVersion;
-
-    programs.awscli = {
-      enable = true;
-      # https://l-lin.github.io/devops/cloud/aws/AWS-CLI-with-Unix-password-manager
-      credentials = {
-        "default" = {
-          "credential_process" = "${pkgs.passage}/bin/passage show garage/nix";
-        };
-      };
-      settings = {
-        "default" = {
-          region = "garage";
-          endpoint_url = "http://nix-cache.s3.muro.ponkila.nix:3900";
-        };
-      };
-    };
 
     accounts.calendar = {
       accounts = {
@@ -82,71 +66,82 @@
       basePath = ".calendar";
     };
 
-    programs.qcal = {
-      enable = true;
-      timezone = config.time.timeZone;
-    };
+    programs = {
+      awscli = {
+        enable = true;
+        # https://l-lin.github.io/devops/cloud/aws/AWS-CLI-with-Unix-password-manager
+        credentials = {
+          "default" = {
+            "credential_process" = "${pkgs.passage}/bin/passage show garage/nix";
+          };
+        };
+        settings = {
+          "default" = {
+            region = "garage";
+            endpoint_url = "http://nix-cache.s3.muro.ponkila.nix:3900";
+          };
+        };
+      };
+      direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
+      fish = {
+        enable = true;
+        loginShellInit = ''
+          set -U fish_greeting
+          set -x IPFS_PATH ${config.services.kubo.dataDir}
+          set -x PATH '${lib.concatStringsSep ":" [
+            "${home.homeDirectory}/.nix-profile/bin"
+            "/run/wrappers/bin"
+            "/etc/profiles/per-user/${home.username}/bin"
+            "/run/current-system/sw/bin"
+            "/nix/var/nix/profiles/default/bin"
+          ]}'
+          test $TERM = "xterm-256color"; and exec tmux
+        '';
+      };
+      fzf.enable = true; # https://andrew-quinn.me/fzf/
+      git = {
+        enable = true;
+        ignores = [
+          ".DS_Store"
+          ".direnv"
+          "node_modules"
+          "result"
+          ".devenv"
+        ];
+      };
+      qcal = {
+        enable = true;
+        timezone = config.time.timeZone;
+      };
+      tmux = {
+        enable = true;
+        baseIndex = 1;
+        plugins = with pkgs.tmuxPlugins; [
+          extrakto # Ctrl+Space+Tab
+          tilish # Option+Enter
+          tmux-fzf # Ctrl+Space+f
+        ];
+        extraConfig = ''
+          set -g @tilish-dmenu 'on'
+          set -g mouse on
 
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-    };
+          bind | split-window -h
+          unbind %
 
-    programs.fish = with config.home-manager.users.juuso; {
-      enable = true;
-      loginShellInit = ''
-        set -U fish_greeting
-        set -x IPFS_PATH ${config.services.kubo.dataDir}
-        set -x PATH '${lib.concatStringsSep ":" [
-          "${home.homeDirectory}/.nix-profile/bin"
-          "/run/wrappers/bin"
-          "/etc/profiles/per-user/${home.username}/bin"
-          "/run/current-system/sw/bin"
-          "/nix/var/nix/profiles/default/bin"
-        ]}'
-        test $TERM = "xterm-256color"; and exec tmux
-      '';
-    };
+          set -g focus-events on
+          set-option -g default-shell "${pkgs.fish}/bin/fish"
 
-    programs.tmux = {
-      enable = true;
-      baseIndex = 1;
-      plugins = with pkgs.tmuxPlugins; [
-        extrakto # Ctrl+Space+Tab
-        tilish # Option+Enter
-        tmux-fzf # Ctrl+Space+f
-      ];
-      extraConfig = ''
-        set -g @tilish-dmenu 'on'
-        set -g mouse on
+          set-option -sg escape-time 10
+          set-option -g default-terminal "tmux-256color"
+          set-option -sa terminal-features ",*:RGB"
 
-        bind | split-window -h
-        unbind %
-
-        set -g focus-events on
-        set-option -g default-shell "${pkgs.fish}/bin/fish"
-
-        set-option -sg escape-time 10
-        set-option -g default-terminal "tmux-256color"
-        set-option -sa terminal-features ",*:RGB"
-
-        TMUX_FZF_LAUNCH_KEY="f"
-      '';
-      shortcut = "Space";
-    };
-
-    # https://andrew-quinn.me/fzf/
-    programs.fzf.enable = true;
-
-    programs.git = {
-      enable = true;
-      ignores = [
-        ".DS_Store"
-        ".direnv"
-        "node_modules"
-        "result"
-        ".devenv"
-      ];
+          TMUX_FZF_LAUNCH_KEY="f"
+        '';
+        shortcut = "Space";
+      };
     };
   };
 
